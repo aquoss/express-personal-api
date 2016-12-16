0ad1012d3fbbe55fe0e15c9ceb2bf5550ce221a5
aquoss <amberquoss@gmail.com>
2016-12-15 16:30:16 -0800

    adds project model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
+//require mongoose
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
-// module.exports.Campsite = require("./campsite.js.example");
+//export models
+module.exports.Project = require('./project.js');
