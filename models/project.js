56c1a4acb4b4aa575b1e6873a87ded0050aa7485
aquoss <amberquoss@gmail.com>
2016-12-15 16:28:23 -0800

    creates project schema and model

---
+++ models/project.js
@@ -0,0 +1,19 @@
+//require mongoose
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+//create project schema
+var ProjectSchema = new Schema({
+  name: String,
+  dateCreated: String,
+  description: String,
+  technologies: Array,
+  deploymentSite: String,
+  screenshot: String
+});
+
+//create model from schema
+var Project = mongoose.model('Project', ProjectSchema);
+
+//export
+module.exports = Project;
