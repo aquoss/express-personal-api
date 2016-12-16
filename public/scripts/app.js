08617804f3722242c2db7579e6ff25ac00180d10
aquoss <amberquoss@gmail.com>
2016-12-16 14:23:18 -0800

    outlines all ajax requests

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,60 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  // responds with all api data
+  $.ajax({
+    method: 'GET',
+    url: 'https://personal-api-aquoss.herokuapp.com/api',
+    success: onSuccess,
+    error: onError
+  })
+
+  // responds with profile data
+  $.ajax({
+    method: 'GET',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/profile',
+    success: onSuccess,
+    error: onError
+    })
+
+  // responds with all projects
+  $.ajax({
+    method: 'GET',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/projects',
+    success: onSuccess,
+    error: onError
+    })
+
+  // responds with one specific project
+  $.ajax({
+    method: 'GET',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/projects/:id',
+    success: onSuccess,
+    error: onError
+    })
+
+  // creates a new project
+  $.ajax({
+    method: 'POST',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/projects',
+    success: onSuccess,
+    error: onError
+    })
+
+  // deletes a project
+  $.ajax({
+    method: 'DELETE',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/projects/:id',
+    success: onSuccess,
+    error: onError
+    })
+
+  // updates a project
+  $.ajax({
+    method: 'PATCH',
+    url: 'https://personal-api-aquoss.herokuapp.com/api/projects/:id',
+    success: onSuccess,
+    error: onError
+    })
 
 });
