677035c4edd06d987b657e8cb08b88a383673c1c
aquoss <amberquoss@gmail.com>
2016-12-15 16:45:25 -0800

    creates seed data for projects model

--- seed.js
+++ seed.js
@@ -1,15 +1,21 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var newProject = {name: "Black Magic",
+  dateCreated: "last week",
+  description: "witch jumps on bats",
+  technologies: ["jQuery", "CSS", "HTML"],
+  deploymentSite: "http://black-magic.bitballoon.com",
+  screenshot: "none"
+  }
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
+db.Project.create(newProject, function(err, project){
+  if (err){
+    return console.log("Error:", err);
+  }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+  console.log("Created new project", project._id)
+  process.exit(); // we're all done! Exit the program.
+})
