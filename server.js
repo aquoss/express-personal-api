0b473a9eceb53f2945e14be6d76b09257a016adf
aquoss <amberquoss@gmail.com>
2016-12-17 23:28:00 -0800

    changes response header to attempt to make delete request work

--- server.js
+++ server.js
@@ -143,7 +143,9 @@ app.delete('/api/projects/:id', function (req, res) {
   //   headers["Access-Control-Allow-Methods"] = POST, GET, PUT, DELETE, OPTIONS;
   //   headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
   // }
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+  res.header({'Access-Control-Allow-Methods':'GET,PUT,PATCH,POST,DELETE',
+            'Access-Control-Allow-Origin':"*",
+            'Access-Control-Allow-Headers':"X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"});
   //get project by id from url params
   db.Project.findOneAndRemove({_id: req.params.id}, function (err, deletedProject) {
     if (err) {
