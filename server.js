0e0bb49032618ca2311c8588ee88c210fb50d545
aquoss <amberquoss@gmail.com>
2016-12-17 23:23:48 -0800

    changes response header for delete request

--- server.js
+++ server.js
@@ -137,6 +137,13 @@ app.post('/api/projects', function (req, res) {
 
 //delete project
 app.delete('/api/projects/:id', function (req, res) {
+  // if (req.method === 'OPTIONS') {
+  //   var headers = {};
+  //   headers["Access-Control-Allow-Origin"] = "*";
+  //   headers["Access-Control-Allow-Methods"] = POST, GET, PUT, DELETE, OPTIONS;
+  //   headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+  // }
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
   //get project by id from url params
   db.Project.findOneAndRemove({_id: req.params.id}, function (err, deletedProject) {
     if (err) {
